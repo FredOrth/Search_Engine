350b45d5c961525de2e96b03e7b8ac55aa9b8d36
Frederik Kølbel <frederik.kolbel@gmail.com>
2026-07-27 19:25:38 +0200

    Setup and initial webserver architecture with Spring

---
+++ src/main/java/app/searchengine/WebPage.java
@@ -0,0 +1,17 @@
+package app.searchengine;
+
+import java.util.List;
+
+public class WebPage {
+
+	private String title;
+	private String url;
+	private List<String> keywords;
+
+	public WebPage(String title, String url, List<String> keywords) {
+
+		this.url = url;
+		this.title = title;
+		this.keywords = keywords;
+	}
+}
---
+++ src/main/java/app/searchengine/WebPageRepository.java
@@ -0,0 +1,5 @@
+package app.searchengine;
+
+public interface WebPageRepository {
+
+}
